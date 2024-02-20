05395a14aa37598707160ebf6aa82cc5719b34c0
Purnima Katiyar <75112217+purnimakatiyar@users.noreply.github.com>
2024-02-21 01:21:10 +0530

    Update config.py

--- src/settings/config.py
+++ src/settings/config.py
@@ -1,17 +1,19 @@
 import json
-
+from os import path
+ 
 queries = None
 prompts = None
 menu = None
+inputs = None
+constant = None
+ 
+path_current_directory = path.dirname(__file__)
+ 
 
-
-filepath = "C:\\Users\\pkatiyar\\OneDrive - WatchGuard Technologies Inc\\Desktop\\API MINOR PROJECT\\src\\config.json"
+filepath = path.join(path_current_directory , "../config.json")
 with open(filepath, "r") as file:
     get_data = json.load(file)
     queries = get_data["queries"]
     menu = get_data["menu"]
     prompts = get_data["prompts"]
     constants = get_data["constants"]
-    
-    
-    
